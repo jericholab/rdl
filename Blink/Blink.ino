a3c0d925fd5f04363d06f6813b9249debc45b037
Jericho Lab <99691909+jericholab@users.noreply.github.com>
2022-04-27 18:30:55 -0400

    Reduced the delay length

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -33,5 +33,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(2000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+  delay(1000);                       // wait for a second
 }
