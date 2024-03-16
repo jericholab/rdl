81fdf6020d1bcd035750003b936cddb36fd24e45
Jericho Lab <99691909+jericholab@users.noreply.github.com>
2024-03-16 17:11:09 -0400

    Add Python scripts from WIRED repo to RDL repo

---
+++ Source Code/README.md
@@ -0,0 +1,25 @@
+PYTHON SCRIPTS
+
+The python scripts allow the integration of the surveillance camera, the infrared camera and the RDL Data Logger into the Orange Pi 4 LTS.
+
+DAEMONS
+
+A shell script (.sh) will activate permanent services (daemons) via configuration files (.services) which run the usual python scripts (e.g. infrared.py).
+
+1- Stop all the WIRED python scripts, if any were running.
+
+2- Open a new terminal in the "/daemons-wired "folder, and execute  "$ sudo ./wired-enable-start-all-daemons.sh". This will copy a few files into the system, enable the daemons, start the scripts and show status.
+
+3- You can verify the scripts are running by verifying the usual output (/logging-folder).
+
+4- If you only want to verify that each python script is running, open a terminal in the "/daemons-wired "folder, and execute  "$ sudo ./wired-status-daemons.sh". Use page up and page down to look at all the lines.
+
+5- Reboot the computer and verify the scripts are still running (since the daemons have been enable, they will start at boot time)
+
+6- If you want to stop and disable all the scripts, execute  "$ sudo ./wired-disable-stop-all-daemons.sh"
+
+7- If you wish to stop temporarily a given script (e.g. infrared camera), "$ systemctl stop infrared.service"  (It WILL restart at next OS reboot)
+
+8- If you wish to stop permanently a given script (e.g. infrared camera), "$ systemctl disable infrared.service"  (It WILL NOT restart at next OS reboot)
+
+Remember to keep the files associated with daemons in their own subfolder.
---
+++ Source Code/WIRED-logging-RDL-USB0.py
@@ -0,0 +1,100 @@
+#jericholab rdl - test script with python with daily file
+
+#!/usr/bin/env python3
+import serial
+from datetime import datetime
+import schedule
+import time
+import shutil
+import os  # library to interact with the operating system
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  #change working directory to the directory containing the script
+
+
+print("RDL-python-logging...")
+DEVICE_NAME = "USB0"
+ser=serial.Serial('/dev/ttyUSB0', 57600) #change the port and baud rate as needed
+
+time.sleep(2)  #test #During initialization of the arduino, the arduino may send random noise.
+
+
+
+# Clear input buffer  (this is an attempt to avoid the initial gibberish)
+ser.reset_input_buffer()   #method 1
+#ser.flushInput()      #method 2 
+#ser.flushOutput()      #method 2
+#time.sleep(2)
+#ser.write(b'quantity\r')
+ser.write(b'reset\r')
+time.sleep(2)
+
+#initializing
+now = datetime.now()
+#formatExpected = "%Y-%m-%d"  #log into different file every day
+formatExpected = "%Y-%m-%d_%H"  #log into different file every hour
+#formatExpected = "%Y-%m-%d_%H:%M"  #log into different file every minute
+now = now.strftime(formatExpected)
+then = now
+
+def createFolder(folder_name):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+        print("Folder created: ", folder_name)
+    else:
+        print("Folder already exists: ", folder_name)
+
+def setUpFolders1():
+    level1 = "./logging-folder"
+    level2 = level1 + "/inprogress"
+    level3 = level2 + "/RDL/"
+    folder_name = level3 + DEVICE_NAME
+    createFolder(level1)
+    createFolder(level2)
+    createFolder(level3)
+    createFolder(folder_name)
+    return folder_name
+
+def setUpFolders2():
+    level1 = "./logging-folder"
+    level2 = level1 + "/tosync"
+    level3 = level2 + "/RDL/"
+    folder_name2 = level3 + DEVICE_NAME
+    createFolder(level1)
+    createFolder(level2)
+    createFolder(level3)
+    createFolder(folder_name2)
+    return folder_name2
+
+folder_name1 = setUpFolders1()  # Run once to create the "inprogress" folder
+folder_name2 = setUpFolders2()  # Run once to create the "readytosync" folder
+destination_path = folder_name2  #where we send the file when it is complete and ready to sync (relative path)
+#destination_path = "./logging-folder/tosync/"  #where we send the file when it is complete and ready to sync (relative path)
+
+while True:
+    now = datetime.now()
+    now = now.strftime(formatExpected)  
+    
+    if (now != then):
+      shutil.move(file_name, destination_path) #move file_name to the /sync_folder
+    
+    #file_name= main_folder + "logging-folder/" + now + ".txt"  #where we save the data as it accumulates (include name and extension)
+    #file_name= "./logging-folder/" + "RDL_" + now + "_USB0" + ".txt"  #where we save the data as it accumulates (include name and extension) (relative path)
+    file_name= folder_name1 + "/RDL_" + now + "_" + DEVICE_NAME + ".txt"  #where we save the data as it accumulates (include name and extension) (relative path)
+    
+    with open(file_name, 'a') as file:
+        try:
+            #data = ser.readline().decode()
+            data = ser.readline().decode('ascii')
+        except UnicodeDecodeError:
+            data = 'x'   #lets try something to handle the error
+        file.write(data)
+        print(data)
+    
+    #time.sleep(1) #to avoid super fast looping
+    then = now
+    file_path = "./logging-folder/"  #where we save the data as it accumulates (relative path)
+    #file_path = main_folder + "logging-folder/"  #where we save the data as it accumulates
+    
+
+    
+    
