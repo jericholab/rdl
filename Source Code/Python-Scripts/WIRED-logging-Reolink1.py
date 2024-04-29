ec7d11503713cf4e72c77d55a3d8682c42196cc8
Jericho Lab <99691909+jericholab@users.noreply.github.com>
2024-04-29 16:41:29 -0400

    Added auto daily foldering

--- Source Code/Python-Scripts/WIRED-logging-Reolink1.py
+++ Source Code/Python-Scripts/WIRED-logging-Reolink1.py
@@ -17,7 +17,6 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))  #change working directory 
 # Set the camera's IP address and login credentials
 CAMERA_NAME = "reolink1"
 CAMERA_IP = "192.168.0.195"
-#CAMERA_IP = "192.168.0.196"
 USERNAME = "admin"
 PASSWORD = "password177"
 
@@ -26,10 +25,15 @@ url = f"http://{CAMERA_IP}/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=wuuPhkmUCeI9WG7
 
 #initializing
 now = datetime.now()
+dailyFolderNow = datetime.now()
 #formatExpected = "%Y-%m-%d_%H"  #log once every hour
 formatExpected = "%Y-%m-%d_%H_%M"  #log once every minute
 now = now.strftime(formatExpected)
 then = now
+#formatDailyFolder = "%Y-%m-%d_%H_%M"  #daily folder (temporarily a minute folder for testing)
+formatDailyFolder = "%Y-%m-%d"  #daily folder
+dailyFolderNow = dailyFolderNow.strftime(formatDailyFolder)
+#folder_name ="0" #initialize to make variable global
 
 def createFolder(folder_name):
     if not os.path.exists(folder_name):
@@ -42,15 +46,20 @@ def setUpFolders():
     level1 = "./logging-folder"
     level2 = level1 + "/tosync"
     level3 = level2 + "/cameras/"
-    folder_name = level3 + CAMERA_NAME
+    level4 = level3 + CAMERA_NAME
+    folder_name = level4 + "/" + dailyFolderNow
     createFolder(level1)
     createFolder(level2)
     createFolder(level3)
+    createFolder(level4)
     createFolder(folder_name)
     return folder_name
 
 def takePicture():
+    folder_name = setUpFolders() #rerun the function to create the daily folder, if applicable
     file_name= folder_name + '/' + now + '.jpg'  #where we save the data as it accumulates (include name and extension)
+
+    
     # Send an HTTP request to the camera's web interface and download the image data
     response = requests.get(url)
     if response.status_code == 200:
@@ -61,7 +70,7 @@ def takePicture():
     else:
         print('Error retrieving snapshot')
 
-folder_name = setUpFolders()  # Run once 
+folder_name = setUpFolders()  # Run once at startup
 print('Take initial picture')            
 takePicture()
 
@@ -69,7 +78,10 @@ takePicture()
 while True:
     now = datetime.now()
     now = now.strftime(formatExpected)  
-    
+    dailyFolderNow = datetime.now()
+    dailyFolderNow = dailyFolderNow.strftime(formatDailyFolder) #update the dailyFolder
+
+
     if (now != then):
         takePicture()     
     
